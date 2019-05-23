5c053788708ec6e7a1023972d04d0ad92f48de3f
Janelle K Hirano <jaahnelle@gmail.com>
2019-05-22 20:55:14 -1000

    added msgCount

--- js/app.js
+++ js/app.js
@@ -10,6 +10,8 @@ spanBox.innerHTML = 11;
 
 //2. In the nav element, target the span element with the id of 'msgCount' and change the number from 0 to 23.
 
+msgCount.innerHTML = 23;
+
 //3. In the div id data section, target the div element with the id of 'fullname' and change Full Name to Ronald McDonald.
 
 //4. In the div id data section, target the div element with the id of 'age' and change Age to 63 Years Old.
