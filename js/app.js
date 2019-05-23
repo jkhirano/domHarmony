e8d6499ec3483d3b39e164d9f08211f1ab0af92b
Janelle K Hirano <jaahnelle@gmail.com>
2019-05-22 21:15:33 -1000

    added hobby

--- js/app.js
+++ js/app.js
@@ -29,6 +29,11 @@ data.appendChild(jobBox);
 
 //6. In the div id data section, create a div with an id of hobbies and give it the following information: Long romantic walks on the beach, candle light Big Mac dinners and tormenting the King.
 
+var hobbyBox = document.createElement('div');
+hobbyBox.id = 'hobbies';
+hobbyBox.innerHTML = 'Long romantic walks on the beach, candle light Big Mac dinners and tormenting the King.';
+data.appendChild(hobbyBox);
+
 //7. In the div id data section, create a div with an id of location and give it the following information: Honolulu, HI.
 
 //8. In the div id data section, create a div with an id of wants and give it the following information: Looking for a Mrs. McDonald.
