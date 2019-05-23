cc3c005ac1a907ad484e508724131dcb4e532990
Janelle K Hirano <jaahnelle@gmail.com>
2019-05-22 21:21:15 -1000

    added wants

--- js/app.js
+++ js/app.js
@@ -43,6 +43,11 @@ data.appendChild(locationBox);
 
 //8. In the div id data section, create a div with an id of wants and give it the following information: Looking for a Mrs. McDonald.
 
+var wantsBox = document.createElement('div');
+wantsBox.id = 'wants';
+wantsBox.innerHTML = 'Looking for a Mrs. McDonald.';
+data.appendChild(wantsBox);
+
 //9. In the div id profile section, create a paragraph element with an id of pro2 and create a your own profile for Ronald (at least 2 sentences).
 
 //10. In the div id matches section, target the first div element with the class name of firstName and change Name to Wendy.
